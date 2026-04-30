63e54e5f46b7b0476c156e1c70735ec3ec6999f3
SAIF2584 <165507875+SAIF2584@users.noreply.github.com>
2026-04-30 11:36:51 +0600

    Update to TensorFlow 2.x and Keras 3.0+ syntax - replace deprecated parameters

--- bssic_Churn_Modelling_ANN01.py
+++ bssic_Churn_Modelling_ANN01.py
@@ -13,20 +13,19 @@ dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13]
 y = dataset.iloc[:, 13]
 
-#Create dummy variables
-geography=pd.get_dummies(X["Geography"],drop_first=True)
-gender=pd.get_dummies(X['Gender'],drop_first=True)
+# Create dummy variables
+geography = pd.get_dummies(X["Geography"], drop_first=True)
+gender = pd.get_dummies(X['Gender'], drop_first=True)
 
 ## Concatenate the Data Frames
-
-X=pd.concat([X,geography,gender],axis=1)
+X = pd.concat([X, geography, gender], axis=1)
 
 ## Drop Unnecessary columns
-X=X.drop(['Geography','Gender'],axis=1)
+X = X.drop(['Geography', 'Gender'], axis=1)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -36,50 +35,57 @@ X_test = sc.transform(X_test)
 
 # Part 2 - Now let's make the ANN!
 
-# Importing the Keras libraries and packages
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LeakyReLU,PReLU,ELU
-from keras.layers import Dropout
+# Importing the TensorFlow and Keras libraries and packages
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LeakyReLU, PReLU, ELU
 
 
 # Initialising the ANN
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim = 6, init = 'he_uniform',activation='relu',input_dim = 11))
+classifier.add(Dense(units=6, kernel_initializer='he_uniform', activation='relu', input_dim=11))
+
+# Adding dropout for regularization
+classifier.add(Dropout(0.2))
 
 # Adding the second hidden layer
-classifier.add(Dense(output_dim = 6, init = 'he_uniform',activation='relu'))
+classifier.add(Dense(units=6, kernel_initializer='he_uniform', activation='relu'))
+
+# Adding dropout for regularization
+classifier.add(Dropout(0.2))
+
 # Adding the output layer
-classifier.add(Dense(output_dim = 1, init = 'glorot_uniform', activation = 'sigmoid'))
+classifier.add(Dense(units=1, kernel_initializer='glorot_uniform', activation='sigmoid'))
 
 # Compiling the ANN
-classifier.compile(optimizer = 'Adamax', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN to the Training set
-model_history=classifier.fit(X_train, y_train,validation_split=0.33, batch_size = 10, nb_epoch = 100)
+model_history = classifier.fit(X_train, y_train, validation_split=0.33, batch_size=10, epochs=100, verbose=1)
 
 # list all data in history
 
 print(model_history.history.keys())
 # summarize history for accuracy
-plt.plot(model_history.history['acc'])
-plt.plot(model_history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.plot(model_history.history['accuracy'])
+plt.plot(model_history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
 # summarize history for loss
 plt.plot(model_history.history['loss'])
 plt.plot(model_history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['train', 'validation'], loc='upper left')
 plt.show()
 
 # Part 3 - Making the predictions and evaluating the model
@@ -89,9 +95,17 @@ y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 cm = confusion_matrix(y_test, y_pred)
 
 # Calculate the Accuracy
-from sklearn.metrics import accuracy_score
-score=accuracy_score(y_pred,y_test)
+score = accuracy_score(y_pred, y_test)
+print(f"Accuracy Score: {score}")
+
+# Print classification report for detailed metrics
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+# Print confusion matrix
+print("\nConfusion Matrix:")
+print(cm)
